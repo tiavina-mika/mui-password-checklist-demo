3700b9bf0030687201ade22f7160175b1f149c69
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-06-22 05:29:37 +0000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -13,38 +13,40 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 6 }}>
-      <Container className="flex flex-row justify-center mt-1">
+      <div className="flex flex-col justify-center items-center gap-12 min-h-[100vh] bg-gray-200">
         <Card className="max-w-[400px] pb-8">
           <CardContent >
-            <div className="flex flex-col gap-2 mb-16 mt-2">
+            <div className="flex flex-col gap-2 mb-12 mt-2">
               <Typography variant="h5">mui-password-checklist</Typography>
               <Typography>Click on the input field and type a password to see the chack list</Typography>
             </div>
             <PasswordChecklist
               value={password}
               onChange={handlePasswordChange}
-              // override class name
-              className='input'
-              // override error messages
-              validationMessages={{
-                minLength: 'Devrait contenir au moins 6 caractères',
-                lowerCase: 'Devrait contenir au moins une lettre minuscule',
-                upperCase: 'Devrait contenir au moins une lettre majuscule',
-                number: 'Devrait contenir au moins un chiffre',
-                specialCharacters: 'Devrait contenir au moins un caractère spécial',
-              }}
-              // override options
-              options={{
-                minLength: 6,
-                allowedSpecialChar: "="
-              }}
+              /**
+               * decomment if overriding the error messages
+               */
+              // validationMessages={{
+              //   minLength: 'Devrait contenir au moins 6 caractères',
+              //   lowerCase: 'Devrait contenir au moins une lettre minuscule',
+              //   upperCase: 'Devrait contenir au moins une lettre majuscule',
+              //   number: 'Devrait contenir au moins un chiffre',
+              //   specialCharacters: 'Devrait contenir au moins un caractère spécial',
+              // }}
+              /**
+               * decomment if overriding the options
+               */
+              // options={{
+              //   minLength: 6,
+              //   allowedSpecialChar: "="
+              // }}
               // override TextFieldProps
               fullWidth
+              placeholder="Enter your password"
             />
           </CardContent>
         </Card>
-      </Container>
+      </div>
     </ThemeProvider>
   )
 }
