9e500bc40831dd4e954342ab15ddab00432f4048
tiavina-mika <tiavinamika@gmail.com>
2024-06-22 07:51:11 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# mui-password-checklist-demo
-A Material-UI password input with list of password validation steps or conditions that should be fulfilled.
+## CodeSandbox mui-password-strength-input demo
+
+A repo to see and test `mui-password-strength-input` features
+<br />
+[GitHub](https://github.com/tiavina-mika/mui-password-strength-input)
+<br />
+[npm](https://www.npmjs.com/package/mui-password-strength-input)
+<br />
+[Live demo](https://mui-password-strength-input.netlify.app)
+
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { Card, CardContent, Container, ThemeProvider, Typography, createTheme } from '@mui/material';
+import PasswordChecklist from 'mui-password-checklist';
+import { ChangeEvent, useState } from 'react';
+
+const theme = createTheme();
+
+const App = () => {
+  const [password, setPassword] = useState<string>('');
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 6 }}>
+      <Container className="flex flex-row justify-center mt-1">
+        <Card className="max-w-[400px] pb-8">
+          <CardContent >
+            <div className="flex flex-col gap-2 mb-16 mt-2">
+              <Typography variant="h5">mui-password-checklist</Typography>
+              <Typography>Click on the input field and type a password to see the chack list</Typography>
+            </div>
+            <PasswordChecklist
+              value={password}
+              onChange={handlePasswordChange}
+              // override class name
+              className='input'
+              // override error messages
+              validationMessages={{
+                minLength: 'Devrait contenir au moins 6 caractères',
+                lowerCase: 'Devrait contenir au moins une lettre minuscule',
+                upperCase: 'Devrait contenir au moins une lettre majuscule',
+                number: 'Devrait contenir au moins un chiffre',
+                specialCharacters: 'Devrait contenir au moins un caractère spécial',
+              }}
+              // override options
+              options={{
+                minLength: 6,
+                allowedSpecialChar: "="
+              }}
+              // override TextFieldProps
+              fullWidth
+            />
+          </CardContent>
+        </Card>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
